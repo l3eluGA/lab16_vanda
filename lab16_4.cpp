a07ddffda1f5e7259d1c8edc6932bf833ed4041b
l3eluGA <rungthiporange@gmail.com>
2023-02-07 16:41:51 +0700

    done

--- lab16_4.cpp
+++ lab16_4.cpp
@@ -17,3 +17,15 @@ int main(){
 	
 	return 0;
 }
+void shuffle(int &a,int &b,int &c,int &d)
+{
+	int n;
+	n=rand()%4;
+	int n1 =a,n2=b,n3=c,n4=d;
+	switch(n)
+	{	case 0: a=b; b=n1;break;
+		case 1: c=d; d=n3;break;
+		case 2: a=c; c=n1;break;
+		case 3: b=d; d=n2;break;
+	}
+}
--- lab16_5.cpp
+++ lab16_5.cpp
@@ -3,7 +3,7 @@
 #include <ctime>
 using namespace std;
 
-void shuffle(int &,int &, int &, int &); //Modify input arguments to pointer 
+void shuffle(int *,int *, int *, int *); 
 
 int main(){
 	int a = 50, b = 100, c = 500, d = 1000;
@@ -11,11 +11,23 @@ int main(){
 	srand(time(0));	
 	
 	for(int i = 0;i < 10;i++){
-	    shuffle(a,b,c,d); //Modify input arguments to pointer 
+	    shuffle(&a,&b,&c,&d);
 	    cout << a << " " << b << " " << c << " " << d << "\n";
 	}
 	
 	return 0;
 }
 
-//Write definition of shuffle() using pointer here 
+void shuffle(int *a,int *b,int *c,int *d)
+{
+	int n;
+	n=rand()%4;
+	int n1 =*a,n2=*b,n3=*c,n4=*d;
+	switch(n)
+	{	case 0: *a=*b; *b=n1;break;
+		case 1: *c=*d; *d=n3;break;
+		case 2: *a=*c; *c=n1;break;
+		case 3: *b=*d; *d=n2;break;
+		default: *c=*d; *d=n3;break;
+	}
+}
